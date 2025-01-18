4c559d463a2745fdc8561b0c1a99c2c6d3284b4e
JoniMora <yoniiran@gmail.com>
2025-01-18 13:33:04 -0300

    Agregar función para representar la lista de amigos

--- app.js
+++ app.js
@@ -9,9 +9,21 @@ function addFriend() {
         friends.push(friend); 
         document.getElementById('friend').value = '';
         console.log(friend);  
+        renderFriendList();
     }
 
     console.log(friends);
+}
+
+function renderFriendList(){
+    const list = document.getElementById('friendList');
+    list.innerHTML = '';
+
+    for(let i = 0; i < friends.length; i++){
+        const li = document.createElement('li');
+        li.textContent = friends[i];
+        list.appendChild(li);
+    }
 }
 
 document.getElementById('friend').addEventListener('keydown', function (event) {
